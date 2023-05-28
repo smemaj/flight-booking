f03230c0eee823fbb0e820ff5ee57ebab97e2370
MEMAJ <sonia.memaj@lhind.dlh.de>
2023-05-29 00:35:27 +0200

    update booking class

--- src/main/java/lhind/flights/booking/model/entity/Booking.java
+++ src/main/java/lhind/flights/booking/model/entity/Booking.java
@@ -1,4 +1,73 @@
 package lhind.flights.booking.model.entity;
 
+import jakarta.persistence.*;
+import lhind.flights.booking.model.enums.BookingStatusEnum;
+
+import java.util.Date;
+import java.util.List;
+
+@Entity
+@Table(name = "booking")
 public class Booking {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Integer id;
+    @Column(name = "booking_time", nullable = false)
+    @Temporal(value = TemporalType.TIME)
+    private Date bookingTime;
+    @ManyToOne
+    @JoinColumn(name = "user_id", referencedColumnName = "id")
+    private MyUser user;
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(
+            name = "booking_flight",
+            joinColumns = @JoinColumn(name = "booking_id", referencedColumnName = "id"),
+            inverseJoinColumns = @JoinColumn(name = "flight_id", referencedColumnName = "id")
+    )
+    private List<Flight> flights;
+    @Column(name = "booking_status")
+    @Enumerated(value = EnumType.STRING)
+    private BookingStatusEnum bookingStatus;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Date getBookingTime() {
+        return bookingTime;
+    }
+
+    public void setBookingTime(Date bookingTime) {
+        this.bookingTime = bookingTime;
+    }
+
+    public MyUser getUser() {
+        return user;
+    }
+
+    public void setUser(MyUser user) {
+        this.user = user;
+    }
+
+    public List<Flight> getFlights() {
+        return flights;
+    }
+
+    public void setFlights(List<Flight> flights) {
+        this.flights = flights;
+    }
+
+    public BookingStatusEnum getBookingStatus() {
+        return bookingStatus;
+    }
+
+    public void setBookingStatus(BookingStatusEnum bookingStatus) {
+        this.bookingStatus = bookingStatus;
+    }
 }
