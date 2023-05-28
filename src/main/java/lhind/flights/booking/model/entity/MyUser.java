27887b21cfc0bdc89465393342cfd2bb978aa654
MEMAJ <sonia.memaj@lhind.dlh.de>
2023-05-28 15:58:56 +0200

    update entities

--- src/main/java/lhind/flights/booking/model/entity/MyUser.java
+++ src/main/java/lhind/flights/booking/model/entity/MyUser.java
@@ -1,4 +1,27 @@
 package lhind.flights.booking.model.entity;
 
+import jakarta.persistence.*;
+
+
+@Entity
+@Table(name = "user")
 public class MyUser {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false, unique = true)
+    private Long id;
+    @Column(name = "first_name", nullable = false)
+    private String firstName;
+    @Column(name = "middle_name")
+    private String middleName;
+    @Column(name = "last_name", nullable = false)
+    private String lastName;
+    @Column(name = "email", nullable = false, unique = true)
+    private String email;
+    @Column(name = "phone")
+    private String phone;
+    @Column(name = "address")
+    private String address;
+
 }
--- src/main/java/lhind/flights/booking/model/entity/Role.java
+++ src/main/java/lhind/flights/booking/model/entity/Role.java
@@ -1,4 +1,51 @@
 package lhind.flights.booking.model.entity;
 
+import jakarta.persistence.*;
+import lhind.flights.booking.model.enums.RoleEnum;
+
+@Entity
+@Table(name = "role")
 public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Integer id;
+    @Enumerated(value = EnumType.STRING)
+    @Column(name = "role", nullable = false)
+    private RoleEnum role;
+    @Column(name = "description")
+    private String description;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public RoleEnum getRole() {
+        return role;
+    }
+
+    public void setRole(RoleEnum role) {
+        this.role = role;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Override
+    public String toString() {
+        return "Role{" +
+                "id=" + id +
+                ", role=" + role +
+                ", description='" + description + '\'' +
+                '}';
+    }
 }
