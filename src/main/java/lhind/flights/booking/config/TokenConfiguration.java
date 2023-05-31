923dcbce46a43acf85e8290bdff3cbcd86dd5570
MEMAJ <sonia.memaj@lhind.dlh.de>
2023-05-31 23:13:07 +0200

    token config

---
+++ src/main/java/lhind/flights/booking/config/TokenConfiguration.java
@@ -0,0 +1,20 @@
+package lhind.flights.booking.config;
+
+
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.crypto.SecretKey;
+
+@Configuration
+public class TokenConfiguration {
+
+    @Bean
+    public SecretKey secretKey() {
+        return Keys.secretKeyFor(SignatureAlgorithm.HS256);
+    }
+
+}
+
