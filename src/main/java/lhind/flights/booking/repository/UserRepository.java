10309a0fd36f3a370314f7ed89f79e2de2cb418f
MEMAJ <sonia.memaj@lhind.dlh.de>
2023-05-31 22:59:12 +0200

    added UserRepository

---
+++ src/main/java/lhind/flights/booking/repository/UserRepository.java
@@ -0,0 +1,14 @@
+package lhind.flights.booking.repository;
+
+
+import lhind.flights.booking.model.entity.MyUser;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<MyUser, Long> {
+
+    Optional<MyUser> findByUsername(String username);
+}
