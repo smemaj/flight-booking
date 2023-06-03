7574f5679a15f9a03bcddc56db5e886166048efe
MEMAJ <sonia.memaj@lhind.dlh.de>
2023-06-03 23:13:21 +0200

    refactoring

--- src/main/java/lhind/flights/booking/mapper/UserMapper.java
+++ src/main/java/lhind/flights/booking/mapper/UserMapper.java
@@ -1,45 +1,45 @@
 package lhind.flights.booking.mapper;
 
-import lhind.flights.booking.model.dto.MyUserDTO;
-import lhind.flights.booking.model.entity.MyUser;
+import lhind.flights.booking.model.dto.UserDTO;
+import lhind.flights.booking.model.entity.User;
 
-public class UserMapper extends AbstractMapper<MyUser, MyUserDTO>{
+public class UserMapper extends AbstractMapper<User, UserDTO>{
     @Override
-    public MyUser toEntity(MyUserDTO myUserDTO) {
-        if (myUserDTO == null) {
+    public User toEntity(UserDTO userDTO) {
+        if (userDTO == null) {
             return null;
         }
-        MyUser myUser = new MyUser();
-        myUser.setId(myUserDTO.getId());
-        myUser.setFirstName(myUserDTO.getFirstName());
-        myUser.setMiddleName(myUserDTO.getMiddleName());
-        myUser.setLastName(myUserDTO.getLastName());
-        myUser.setUsername(myUserDTO.getUsername());
-        //myUser.setEmail(myUserDTO.getEmail());
-        myUser.setPassword(myUserDTO.getPassword());
-        myUser.setPhone(myUserDTO.getPhone());
-        myUser.setAddress(myUserDTO.getAddress());
-        myUser.setRoles(myUserDTO.getRoles());
-        return myUser;
+        User user = new User();
+        user.setId(userDTO.getId());
+        user.setFirstName(userDTO.getFirstName());
+        user.setMiddleName(userDTO.getMiddleName());
+        user.setLastName(userDTO.getLastName());
+        user.setUsername(userDTO.getUsername());
+        //user.setEmail(userDTO.getEmail());
+        user.setPassword(userDTO.getPassword());
+        user.setPhone(userDTO.getPhone());
+        user.setAddress(userDTO.getAddress());
+        user.setRoles(userDTO.getRoles());
+        return user;
 
     }
 
     @Override
-    public MyUserDTO toDto(MyUser myUser) {
-        if(myUser == null){
+    public UserDTO toDto(User user) {
+        if(user == null){
             return null;
         }
-        MyUserDTO myUserDTO =new MyUserDTO();
-        myUserDTO.setId(myUser.getId());
-        myUserDTO.setFirstName(myUser.getFirstName());
-        myUserDTO.setMiddleName(myUser.getMiddleName());
-        myUserDTO.setLastName(myUser.getLastName());
-        myUserDTO.setUsername(myUser.getUsername());
-        //myUserDTO.setEmail(myUser.getEmail());
-        myUserDTO.setPassword(myUser.getPassword());
-        myUserDTO.setPhone(myUser.getPhone());
-        myUserDTO.setAddress(myUser.getAddress());
-        myUserDTO.setRoles(myUser.getRoles());
-        return myUserDTO;
+        UserDTO userDTO =new UserDTO();
+        userDTO.setId(user.getId());
+        userDTO.setFirstName(user.getFirstName());
+        userDTO.setMiddleName(user.getMiddleName());
+        userDTO.setLastName(user.getLastName());
+        userDTO.setUsername(user.getUsername());
+        //userDTO.setEmail(user.getEmail());
+        userDTO.setPassword(user.getPassword());
+        userDTO.setPhone(user.getPhone());
+        userDTO.setAddress(user.getAddress());
+        userDTO.setRoles(user.getRoles());
+        return userDTO;
     }
 }
