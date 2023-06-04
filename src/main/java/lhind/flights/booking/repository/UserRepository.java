91599b82b1f4663be0eb930bc317e637142e5a62
MEMAJ <sonia.memaj@lhind.dlh.de>
2023-06-04 15:50:37 +0200

    find users for specific flight

--- src/main/java/lhind/flights/booking/repository/UserRepository.java
+++ src/main/java/lhind/flights/booking/repository/UserRepository.java
@@ -3,12 +3,20 @@ package lhind.flights.booking.repository;
 
 import lhind.flights.booking.model.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
     Optional<User> findByUsername(String username);
+
+    @Query(value = "SELECT C.* " +
+            "from (booking as A inner join booking_flight as B on A.id = B.booking_id) " +
+            "inner join user as C on C.id = A.user_id where flight_id = :id", nativeQuery = true)
+    List<User> findByFlightId(Long id);
+
 }
