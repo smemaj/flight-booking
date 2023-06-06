f264e748c48988bb94b0a0bcca4349740f1299de
MEMAJ <sonia.memaj@lhind.dlh.de>
2023-06-06 03:23:16 +0200

    added DTO for decline reason

---
+++ src/main/java/lhind/flights/booking/model/dto/ReasonDTO.java
@@ -0,0 +1,14 @@
+package lhind.flights.booking.model.dto;
+
+
+public class ReasonDTO {
+    private String reason;
+
+    public String getReason() {
+        return reason;
+    }
+
+    public void setReason(String reason) {
+        this.reason = reason;
+    }
+}
